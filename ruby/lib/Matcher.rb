d5730e27c44cc058880d948078bec45ac7b1d1d6
Lucas <lucasleonardo1996@hotmail.com>
2018-09-06 17:38:55 -0300

    Agregado el matcher de listas para que funcione con combinaciones de symbols y valores, y testeado

--- ruby/lib/Matcher.rb
+++ ruby/lib/Matcher.rb
@@ -1,13 +1,39 @@
- class Symbol
-    def call(a)
-      true
-    end
- end
+#Defino el metodo call en los symbols
+class Symbol
+  def call(a)
+    true
+  end
+end
 
- def val(expected)
-   lambda {|gotten| gotten == expected}
- end
+#Defino los matchers basicos
+def val(expected)
+  lambda {|gotten| gotten == expected}
+end
 
- def type(expected)
-   lambda {|gotten| gotten.is_a?(expected)}
- end
+def type(expected)
+  lambda {|gotten| gotten.is_a?(expected)} # kind_of te retorna true si es una subclase de expected
+end
+
+def list(expected, with_size = true) # supuestamente esto lo hace opcional
+  lambda {|gotten| (with_size ? gotten.size == expected.size : true) && are_equivalents(expected, gotten)}
+  # Lo que no me gusta de esto es que el metodo are_equivalents sigue siendo visible, si lo defino como
+  # self.are_equivalents ya no es visible, pero no puedo correrlo desde la lambda
+end
+
+private
+def are_equivalents(expected, gotten)
+  # alternativas :
+  #   comparision <=>
+  #   == (pero tiene en cuenta la cantidad)
+  #  drop o drop_while y una logica media procedural
+  #  each o each_index y una logica tambien media procedural
+  false if expected.size > gotten.size
+
+  firsts = gotten.first(expected.size)
+
+  result = false
+
+  expected.each_with_index {|elem, index| type(Symbol).call(elem) ? result = elem.call(firsts.at(index)) :  result = firsts.at(index) == elem}
+
+  result
+end
--- ruby/spec/test_spec.rb
+++ ruby/spec/test_spec.rb
@@ -7,6 +7,10 @@ describe 'matcher tests' do
       it 'type(Integer).call no deberia funcionar sin parametros' do
         expect {type(Integer).call}.to raise_error ArgumentError
       end
+
+      it 'esto no deberia ser visible!!!!!' do
+        expect {are_equivalents([1,2,3], [1,2,3,4])}.to raise_error NoMethodError
+      end
     end
 
     describe 'symbol-test' do
@@ -32,6 +36,49 @@ describe 'matcher tests' do
 
       it 'type(Symbol).call(:a) debe ser true' do
         expect(type(Symbol).call(:a)).to be true
+      end
+
+      it 'type(Module).call(Comparable) debe ser true' do
+        expect(type(Module).call(Comparable)).to be true
+      end
+    end
+
+    describe 'list-test' do
+      let(:array_pattern1) {[1, 2, 3, 4]}
+      let(:array_pattern2) {[1, 2, 3]}
+      let(:array_pattern3) {[:a, :b, :c, :d]}
+      let(:array_pattern4) {[:a, :b, 2, :a]}
+
+      it 'al pasarle un array de cuatro siendo que espera ese mismo y tiene en cuenta el tamanio,
+          deberia devolver true' do
+        expect(list(array_pattern1, true).call([1, 2, 3, 4])).to be true # es un objeto distinto a proposito
+      end
+
+      it 'al pasarle un array de cuatro siendo que espera ese mismo y NO tiene en cuenta el tamanio,
+          deberia devolver true' do
+        expect(list(array_pattern1, false).call([1, 2, 3, 4])).to be true # es un objeto distinto a proposito
+      end
+
+      it 'al pasarle lo que espera pero al reves deberia ser false, sin importar el tamanio,
+          porque sino no anda bien la busqueda de los primeros N elementos' do
+        expect(list(array_pattern1, true).call([4,3,2,1])).to be false
+      end
+
+      it 'al pasarle un array con todos symbols deberia dar true' do
+        expect(list(array_pattern3, true).call([1,2,3,4])).to be true
+      end
+
+      it 'al pasarle un array con symbols y numeros deberia dar true' do
+        expect(list(array_pattern4, true).call([1,2,3,4])).to be true
+      end
+
+      it 'al pasarle un array con symbols y numeros (o solo symbols) teniendo en cuenta el tamanio y evaluarlo
+         con un array mas chico deberia dar false' do
+        expect(list(array_pattern4, true).call([1,2,3])).to be false
+      end
+
+      it 'si no se especifica match size no deberia romper y deberia considerarlo true, y este test deberia dar true' do
+        expect(list(array_pattern4).call([1,2,3,4])).to be true
       end
     end
   end
