9817fd1eb5c225aeefe99df0f55202ff1eb98dea
Lucas <lucasleonardo1996@hotmail.com>
2018-09-06 21:02:33 -0300

    Arreglo la comparacion de listas y algunos de sus tests + agrego las operaciones and, or y not y sus test andando
    Deje unos fixme con algunos problemas que tengo que plantear a los demas!!!

--- ruby/lib/Matcher.rb
+++ ruby/lib/Matcher.rb
@@ -1,44 +1,54 @@
+
+module ConcatenableOperations
+  def and(first_matcher, *other_matchers)
+    lambda { |gotten| self.call(gotten) && first_matcher.call(gotten) && other_matchers.all? {|matcher| matcher.call(gotten)}}
+  end
+
+  def or(first_matcher, *other_matchers)
+    lambda {|gotten| self.call(gotten) || first_matcher.call(gotten) || other_matchers.any? {|matcher| matcher.call(gotten)}}
+  end
+
+  def not
+    lambda {|gotten| !self.call(gotten)}
+  end
+end
+
 #Defino el metodo call en los symbols
 class Symbol
   def call(a)
     true
   end
+
+  include ConcatenableOperations
+  # TODO: ESTO NO ME GUSTA, pero si quiero lograr la interfaz :un_symbol.and(type(Class), etc...), medio como que por ahora no queda otra
 end
 
-#Defino los matchers basicos
+#Defino los matchers primitivos
 def val(expected)
-  lambda {|gotten| gotten == expected}
+  lambda {|gotten| gotten == expected}.extend(ConcatenableOperations)
 end
 
 def type(expected)
-  lambda {|gotten| gotten.is_a?(expected)} # kind_of te retorna true si es una subclase de expected
+  lambda {|gotten| gotten.is_a?(expected)}.extend(ConcatenableOperations) # kind_of te retorna true si es una subclase de expected
 end
 
 def list(expected, with_size = true) # supuestamente esto lo hace opcional
-  lambda {|gotten| (with_size ? gotten.size == expected.size : true) && are_equivalents(expected, gotten)}
-  # Lo que no me gusta de esto es que el metodo are_equivalents sigue siendo visible, si lo defino como
-  # self.are_equivalents ya no es visible, pero no puedo correrlo desde la lambda
+  lambda {|gotten| gotten.is_a?(Array) && (with_size ? gotten.size == expected.size : true) && are_equivalents(expected, gotten)}.extend(ConcatenableOperations)
+  # TODO: Lo que no me gusta de esto es que el metodo are_equivalents sigue siendo visible, si lo defino como self.are_equivalents ya no es visible, pero no puedo correrlo desde la lambda
 end
 
 private
 def are_equivalents(expected, gotten)
-  # alternativas :
-  #   comparision <=>
-  #   == (pero tiene en cuenta la cantidad)
-  #  drop o drop_while y una logica media procedural
-  #  each o each_index y una logica tambien media procedural
   false if expected.size > gotten.size
 
   firsts = gotten.first(expected.size)
 
-  result = false
-
-  expected.each_with_index {|elem, index| type(Symbol).call(elem) ? result = elem.call(firsts.at(index)) :  result = firsts.at(index) == elem}
-
-  result
+  expected.each_with_index {|elem, index| elem.respond_to?(:call) ? (return false unless elem.call(firsts.at(index))) :  (return false unless firsts.at(index) == elem)}
+  # FIXME: esto de arriba no me gusta porque estoy diciendo "che, si esta cosa que me pasaste responde a call mandale mecha...", podria generar problemas?
+  true # FIXME: Hay una forma de no tener que devolver true asi ?
 end
 
 def duck(first, *others)
-  lambda {|gotten| (gotten.respond_to? first) && others.all? {|method_name| gotten.respond_to? method_name} }
-  # hay una forma mas linda de hacer esto de los parametros variables ?
+  lambda {|gotten| (gotten.respond_to? first) && others.all? {|method_name| gotten.respond_to? method_name} }.extend(ConcatenableOperations)
+  # TODO: hay una forma mas linda de hacer esto de los parametros variables ?
 end
--- ruby/spec/test_spec.rb
+++ ruby/spec/test_spec.rb
@@ -1,5 +1,5 @@
-describe 'matcher tests' do
-    describe 'syntax-test' do
+describe 'matcher primitives tests' do
+    describe 'syntax-and-usage-test' do
       it '1.call(2) no deberia funcionar, si esta bien armado el fw' do
         expect {1.call(2)}.to raise_error NoMethodError
       end
@@ -8,8 +8,12 @@ describe 'matcher tests' do
         expect {type(Integer).call}.to raise_error ArgumentError
       end
 
-      it 'esto no deberia ser visible!!!!!' do
+      it 'esto no deberia ser visible!!!!!' do # FIXME: Problema... ensucia la interfaz de main y el metodo are_equivalents es accesible de todos lados.
         expect {are_equivalents([1,2,3], [1,2,3,4])}.to raise_error NoMethodError
+      end
+
+      it 'una lambda cualquiera no deberia tener el metodo and' do
+        expect { lambda {}.and(lambda {})}.to raise_error NoMethodError
       end
     end
 
@@ -26,6 +30,10 @@ describe 'matcher tests' do
 
       it 'val(5).call(5) debe dar true' do
         expect(val(5).call(5)).to be true
+      end
+
+      it 'val(5).call(char 5) deberia dar falso' do
+        expect(val(5).call('5')).to be false
       end
     end
 
@@ -47,8 +55,12 @@ describe 'matcher tests' do
       let(:array_pattern1) {[1, 2, 3, 4]}
       let(:array_pattern2) {[1, 2, 3]}
       let(:array_pattern3) {[:a, :b, :c, :d]}
-      let(:array_pattern4) {[:a, :b, 2, :a]}
+      let(:array_pattern4) {[:a, 2, :b, :c]}
+      let(:array_pattern5) {[type(Fixnum), :a, val(3)]}
 
+      it 'al pasarle algo que no es una lista deberia dar false' do # TODO : QUE PASA CON LOS HASHES Y LAS DEMAS COSAS ?
+        expect(list([],false).call(Module)).to be false
+      end
       it 'al pasarle un array de cuatro siendo que espera ese mismo y tiene en cuenta el tamanio,
           deberia devolver true' do
         expect(list(array_pattern1, true).call([1, 2, 3, 4])).to be true # es un objeto distinto a proposito
@@ -68,7 +80,7 @@ describe 'matcher tests' do
         expect(list(array_pattern3, true).call([1,2,3,4])).to be true
       end
 
-      it 'al pasarle un array con symbols y numeros deberia dar true' do
+      it 'al pasarle un array con symbols y numeros deberia dar true, cuando se evalua con un array con los elementos en el orden esperado' do
         expect(list(array_pattern4, true).call([1,2,3,4])).to be true
       end
 
@@ -80,37 +92,103 @@ describe 'matcher tests' do
       it 'si no se especifica match size no deberia romper y deberia considerarlo true, y este test deberia dar true' do
         expect(list(array_pattern4).call([1,2,3,4])).to be true
       end
+
+      it 'al pasarle una lista con matchers deberia andar' do
+        expect(list(array_pattern5).call([1,2,3])).to be true
+      end
     end
 
     describe 'duck-test' do
-      #before do
-      #  pepita = Object.new
-      #  pepita.define_singleton_method(:volar){}
-      #  pepita.define_singleton_method(:comer){}
-      #end
-      class Pepita
-        def volar
-        end
-        def comer
-        end
+      let(:some_array) { Array.new }
+
+      it 'un array entiende each' do
+        expect(duck(:each).call(some_array)).to be true
       end
 
-      let(:pepita) { Pepita.new }
-
-      it 'pepita entiende volar' do
-        expect(duck(:volar).call(pepita)).to be true
+      it 'un array entiende each, all?' do
+        expect(duck(:each, :all?).call(some_array)).to be true
       end
 
-      it 'pepita entiende volar y comer' do
-        expect(duck(:volar, :comer).call(pepita)).to be true
+      it 'un array entiende each, all? y any?' do
+        expect(duck(:each, :all?, :any?).call(some_array)).to be true
       end
 
-      it 'pepita no entiende salto_ninja' do
-        expect(duck(:salto_ninja).call(pepita)).to be false
+      it 'un array no entiende salto_ninja' do
+        expect(duck(:salto_ninja).call(some_array)).to be false
       end
 
-      it 'pepita entiende volar pero no entiende salto_ninja, debe dar false' do
-        expect(duck(:volar, :salto_ninja).call(pepita)).to be false
+      it 'un array entiende any? y all? pero no entiende salto_ninja, debe dar false' do
+        expect(duck(:any?, :all?, :salto_ninja).call(some_array)).to be false
       end
     end
+end
+
+describe 'combinations tests' do
+  let(:empy_array) { Array.new }
+  let(:an_array) {[1,2,3]}
+
+  describe 'and-test' do
+    it 'deberia poder combinar dos matchers sencillos con and y deberia devolver true al evaluarlos' do
+      expect(type(Integer).and(val(3)).call(3)).to be true
+    end
+
+    it 'deberia poder combinar tres matchers sencillos y dar true al evaluarlos' do
+      expect(:a.and(type(Array), list([1,2,3])).call(an_array)).to be true
+    end
+
+    it 'deberia poder combinar dos matchers sencillos y deberia dar false' do
+      expect(list([:a, 1, 3]).and(type(Array)).call(an_array)).to be false
+      # este caso me hizo darme cuenta que estaba resolviendo mal la comparacion en el patron de listas
+    end
+
+    it 'deberia poder combinar varios matchers y deberia dar false' do
+      expect(type(Array).and(:a, val(4)).call(4)).to be false
+    end
   end
+
+  describe 'or-test' do
+    it 'deberia poder combinar dos matchers sencillos con or y deberia dar true' do
+      expect(type(Array).or(type(Comparable)).call(3)).to be true
+    end
+
+    it 'deberia poder combinar varios matchers y deberia dar true' do
+      expect(type(Integer).or(list([1, :a], false), val(1)).call(an_array)).to be true
+    end
+
+    it 'deberia poder combinar dos matchers sencillos y dar false' do
+      expect(type(Integer).or(val(2)).call(an_array)).to be false
+    end
+
+    it 'deberia poder combinar varios matchers y dar false' do
+      expect(val(2).or(list([1,2]), type(Class)).call(Object.new)).to be false
+    end
+  end
+
+  describe 'not-test' do
+    it 'cualquier symbol negado deberia devolver false' do
+      expect(:a.not.call("g")).to be false
+    end
+
+    it 'al preguntar si un numero es entero deberia decir que no' do
+      expect(type(Integer).not.call(2)).to be false
+    end
+
+    it 'al preguntar si el patron de lista coincide debe decir que no' do
+      expect(list([1, :a], false).not.call(an_array)).to be false
+    end
+
+    it 'al preguntar si una clase es de tipo string debe decir que si' do
+      expect(type(String).not.call(Module)).to be true
+    end
+
+    it 'al preguntar si un array entiende all? debe decir que no' do
+      expect(duck(:all?).not.call(an_array)).to be false
+    end
+  end
+
+  describe 'mixed-combinations-test' do
+    it 'bueno, deberia decir que si, es el caso que plantea el enunciado' do
+      expect(list([duck(:+).and(type(Fixnum), :x), :y.or(val(4)), duck(:+).not]).call([1, 2, Object.new])).to be true
+    end
+  end
+end
