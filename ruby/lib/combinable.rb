1bf1335ca22d0beb2ca064bd00235406b9cff1d3
Fernando Besteiro <fernando.besteiro@southworks.com>
2018-09-09 00:44:09 -0300

    Finalicé los tests de la segunda parte del TP

--- ruby/lib/combinable.rb
+++ ruby/lib/combinable.rb
@@ -38,8 +38,8 @@ module Combinable
     # In order to write down a friendly error, we get caller method's name and inform the user if sending no args.
     caller_method = "Combinable." + caller[0][/`.*'/][1..-2] + "()"
 
-    raise "Invalid number of arguments used for #{caller_method}(). At least one required." if elems.length < 1
-    raise "Invalid elements sent to #{caller_method}. Only Combinable elements allowed." unless elems.all? { | elem | elem.is_a?(Combinable) }
+    raise ArgumentError.new "Invalid number of arguments used for #{caller_method}(). At least one required." if elems.length < 1
+    raise ArgumentError.new "Invalid elements sent to #{caller_method}. Only Combinable elements allowed." unless elems.all? { | elem | elem.is_a?(Combinable) }
   end
 end
 
--- ruby/spec/test_spec.rb
+++ ruby/spec/test_spec.rb
@@ -1,5 +1,3 @@
-
-
 describe 'Part 1 - Matchers' do
   describe 'Variable matcher' do
     it 'variable matcher should be true' do
@@ -81,6 +79,14 @@ describe 'Part 1 - Matchers' do
     it 'list matcher should work but return false when sending pattern with more than N elements' do
       expect(list([1, 2, 3, 4, 5], true).call(an_array)).to be false
     end
+
+    it 'list should be false if sending a number as pattern list' do
+      expect(list(1).call(an_array)).to be false
+    end
+
+    it 'list should be false if sending a number as caller list' do
+      expect(list([1, 2]).call(1)).to be false
+    end
   end
 
   describe 'Duck matcher' do
@@ -115,6 +121,72 @@ describe 'Part 1 - Matchers' do
 
     it 'duck matcher should find to_s for an object' do
       expect(duck(:to_s).call(Object.new)).to be true
+    end
+  end
+end
+
+describe 'Part 2 - Combinators' do
+  describe 'and combinator' do
+    it '1 should be both an integer and comparable' do
+      expect(type(Integer).and(type(Comparable)).call(1)).to be true
+    end
+
+    it 'A simple array should not be comparable' do
+      expect(type(Comparable).and(type(Array)).call([])).to be false
+    end
+
+    it '1 should be an integer with value 1 that understands plus and minus' do
+      expect(type(Integer).and(val(1), duck(:+, :-)).call(1)).to be true
+    end
+
+    it 'and should explode if no args sent' do
+      expect { :anything.and() }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe 'or combinator' do
+    it '1 should be value 1 or a string' do
+      expect(val(1).or(type(String)).call(1)).to be true
+    end
+
+    it 'A symbol should be bound to 1 or value should be 10' do
+      expect(:a_symbol.or(val(10)).call(1)).to be true
+    end
+
+    it '5 should not be a String or be 7 or understand kamehameha' do
+      expect(type(String).or(val(7), duck(:kamehameha)).call(5)).to be false
+    end
+
+    it 'or should explode if no args sent' do
+      expect { duck(:something).or() }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe 'not combinator' do
+    it '5 should not be 5 if negated' do
+      expect(val(5).not.call(5)).to be false
+    end
+
+    it ':symbol should be a symbol if negated twice' do
+      expect(type(Symbol).not.not.call(:symbol)).to be true
+    end
+
+    it 'not should fail if args sent' do
+      expect { val(3).not(1) }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe 'complex combinators' do
+    it 'Chau shouldnt be a symbol or Hola or integer and bound to a symbol' do
+      expect(type(Symbol).not.or(val("Hola"), type(Integer).and(:a_symbol)).call("Chau")).to be true
+    end
+
+    it '3 should understand * and be Integer and Comparable but should not be 5 and should be bound to :asd or used with list' do
+      expect(duck(:*).and(type(Integer), type(Comparable), val(5).not, :asd.or(list([3]))).call(3)).to be true
+    end
+
+    it 'List should match pattern and be of type Enumerable and not understand :potato negated' do
+      expect(list([:xin_zhao, :malphite, :kindred], false).and(type(Enumerable), duck(:potato).not).not.call([100, 101, 102, 103, 104, 105])).to be false
     end
   end
 end
