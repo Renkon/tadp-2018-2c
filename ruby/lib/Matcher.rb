7083ad85957fa5782bee200e9c5aacdc9f6826bc
Lucas <lucasleonardo1996@hotmail.com>
2018-09-13 21:36:26 -0300

    Arrancando el refactor del punto 3 desde cero, testeados y andando Symbol y val

--- ruby/lib/Matcher.rb
+++ ruby/lib/Matcher.rb
@@ -1,63 +1,22 @@
-
 module ConcatenableOperations
   def and(first_matcher, *other_matchers)
-    #PrimitiveMatcher.evaluates { |gotten| self.call(gotten) && first_matcher.call(gotten) && other_matchers.all? {|matcher| matcher.call(gotten)}}
-    ComplexMatcher.evaluates_and(self, first_matcher, other_matchers)
+    lambda { |gotten| self.call(gotten) && first_matcher.call(gotten) && other_matchers.all? {|matcher| matcher.call(gotten)}}
   end
 
   def or(first_matcher, *other_matchers)
-    PrimitiveMatcher.evaluates {|gotten| self.call(gotten) || first_matcher.call(gotten) || other_matchers.any? {|matcher| matcher.call(gotten)}}
-    #ComplexMatcher.evaluates_or(self, second_matcher, other_matchers)
+    lambda {|gotten| self.call(gotten) || first_matcher.call(gotten) || other_matchers.any? {|matcher| matcher.call(gotten)}}
   end
 
   def not
-    PrimitiveMatcher.evaluates {|gotten| !self.call(gotten)}
-    #ComplexMatcher.evaluates_not(self)
+    lambda {|gotten| !self.call(gotten)}
   end
-end
-
-module SymbolDictionary
-  attr_accessor :symbol_dictionary
-  #private
-  def obtain_expected
-    self.binding.local_variable_get(:expected)
-  end
-
-  #private
-  def try_to_add_symbols_to_dictionary(gotten)
-    self.symbol_dictionary= Hash.new if self.symbol_dictionary.nil?
-
-    expected = self.obtain_expected
-
-    if expected.is_a? Enumerable
-      try_to_add_symbols_for_enumerable(gotten)
-    else
-      if expected.is_a? Symbol
-        self.symbol_dictionary[expected.to_sym] = gotten
-      end
-    end
-
-    puts 'Ahi va el contenido del diccionario : ' + self.symbol_dictionary.to_s
-  end
-
-  #private
-  def try_to_add_symbols_for_enumerable(gotten)
-    self.obtain_expected.each_with_index do |value, index|
-      if value.is_a? Symbol
-        self.symbol_dictionary[value.to_sym] = gotten.at(index)
-      end
-    end
-  end
-end
-
-module ComplexSymbolDictionary
-  attr_accessor :symbol_dictionary
 end
 
 #Defino el metodo call en los symbols
 class Symbol
-  def call(a)
-    true # FIXME aca falta hacer que el symbol bindee variables... todavia no estaria ni planteado...
+  def call(a, symbol_dictionary = Hash.new)
+    symbol_dictionary[self] = a
+    true
   end
 
   include ConcatenableOperations
@@ -65,75 +24,21 @@ class Symbol
 end
 
 #Defino los matchers primitivos
-module PrimitiveMatcher
-  def self.evaluates(&evaluator)
-    evaluator.extend(ConcatenableOperations, SymbolDictionary)
-
-    evaluator.instance_eval do
-      original_method = self.public_method(:call)
-      define_singleton_method(:original_call) {|gotten| original_method.call(gotten)}
-    end
-
-    evaluator.define_singleton_method(:call) do |gotten|
-      result = self.original_call(gotten)
-      self.try_to_add_symbols_to_dictionary(gotten) # if result
-      result
-    end
-
-    evaluator
-  end
-end
-
-module ComplexMatcher
-  def self.evaluates_and(first, second, *others)
-    matchers = ([first, second].zip(others)).flatten
-    evaluator = Proc.new {}
-    #evaluator.instance_eval do
-     # self.instance_variable_set(:@symbol_dictionary, Hash.new)
-      #def symbol_dictionary
-       # symbol_dictionary
-      #end
-
-      #matchers
-      #def matchers
-      #  if matchers.nil?
-      #    Array.new
-      #  end
-      #  matchers
-      #end
-
-    #evaluator.instance_variable_set(:@symbol_dictionary, Hash.new)
-    #evaluator.define_singleton_method(:symbol_dictionary) do
-    #  self.instance_variable_get(:@symbol_dictionary)
-    #end
-
-    evaluator.extend(ComplexSymbolDictionary)
-
-    evaluator.define_singleton_method(:call) do |gotten|
-      p 'estoy en el call'
-      final_result = true
-      evaluator.matchers.each do |matcher|
-        result = matcher.call(gotten)
-        evaluator.symbol_dictionary.merge(matcher.symbol_dictionary)
-        final_result = result unless result
-      end
-      puts 'Ahi va el diccionario del and: ' + evaluator.symbol_dictionary.inspect
-      puts 'El resultado final del and : ' + final_result
-      return final_result
-    end
-  end
-end
-
 def val(expected)
-  PrimitiveMatcher.evaluates {|gotten| gotten == expected}
+  lambda do |gotten, symbol_dictionary = Hash.new|
+    if expected.is_a?(Symbol)
+      symbol_dictionary[expected] = gotten
+    end
+    gotten == expected
+  end.extend(ConcatenableOperations)
 end
 
 def type(expected)
-  PrimitiveMatcher.evaluates {|gotten| gotten.is_a?(expected)} # kind_of te retorna true si es una subclase de expected
+  lambda {|gotten| gotten.is_a?(expected)}.extend(ConcatenableOperations) # kind_of te retorna true si es una subclase de expected
 end
 
 def list(expected, with_size = true) # supuestamente esto lo hace opcional
-  PrimitiveMatcher.evaluates {|gotten| expected.is_a?(Enumerable) && gotten.is_a?(Enumerable) && (with_size ? gotten.size == expected.size : true) && are_equivalents(expected, gotten)}
+  lambda {|gotten| expected.is_a?(Enumerable) && gotten.is_a?(Enumerable) && (with_size ? gotten.size == expected.size : true) && are_equivalents(expected, gotten)}.extend(ConcatenableOperations)
   # TODO: Lo que no me gusta de esto es que el metodo are_equivalents sigue siendo visible, si lo defino como self.are_equivalents ya no es visible, pero no puedo correrlo desde la lambda
 end
 
@@ -148,11 +53,7 @@ def are_equivalents(expected, gotten)
   true # FIXME: Hay una forma de no tener que devolver true asi ?
 end
 
-def duck(*expected)
-  PrimitiveMatcher.evaluates {|gotten| expected.all? {|method_name| gotten.respond_to? method_name} }
+def duck(first, *others)
+  lambda {|gotten| (gotten.respond_to? first) && others.all? {|method_name| gotten.respond_to? method_name} }.extend(ConcatenableOperations)
   # TODO: hay una forma mas linda de hacer esto de los parametros variables ?
 end
-
-#def with(matchers)
-#  matchers.get_symbols.each {|symbol| symbol}
-#end
--- ruby/spec/test_spec.rb
+++ ruby/spec/test_spec.rb
@@ -1,128 +1,145 @@
 describe 'matcher primitives tests' do
-    describe 'syntax-and-usage-test' do
-      it '1.call(2) no deberia funcionar, si esta bien armado el fw' do
-        expect {1.call(2)}.to raise_error NoMethodError
-      end
-
-      it 'type(Integer).call no deberia funcionar sin parametros' do
-        expect {type(Integer).call}.to raise_error ArgumentError
-      end
-
-      it 'esto no deberia ser visible!!!!!' do # FIXME: Problema... ensucia la interfaz de main y el metodo are_equivalents es accesible de todos lados.
-        expect {are_equivalents([1,2,3], [1,2,3,4])}.to raise_error NoMethodError
-      end
-
-      it 'una lambda cualquiera no deberia tener el metodo and' do
-        expect { lambda {}.and(lambda {})}.to raise_error NoMethodError
-      end
+  describe 'syntax-and-usage-test' do
+    it '1.call(2) no deberia funcionar, si esta bien armado el fw' do
+      expect {1.call(2)}.to raise_error NoMethodError
     end
 
-    describe 'symbol-test' do
-      it ':a.call("b") debe andar y devolver true' do
-        expect(:a.call("b")).to be true
-      end
+    it 'type(Integer).call no deberia funcionar sin parametros' do
+      expect {type(Integer).call}.to raise_error ArgumentError
     end
 
-    describe 'val-test' do
-      it 'val(5).call(2) debe dar falso' do
-        val(3).obtain_expected
-        expect(val(5).call(2)).to be false
-      end
-
-      it 'val(5).call(5) debe dar true' do
-        expect(val(5).call(5)).to be true
-      end
-
-      it 'val(5).call(char 5) deberia dar falso' do
-        expect(val(5).call('5')).to be false
-      end
+    it 'esto no deberia ser visible!!!!!' do # FIXME: Problema... ensucia la interfaz de main y el metodo are_equivalents es accesible de todos lados.
+      expect {are_equivalents([1,2,3], [1,2,3,4])}.to raise_error NoMethodError
     end
 
-    describe 'type-test' do
-      it 'type(Symbol).call(2) debe ser false' do
-        expect(type(Symbol).call(2)).to be false
-      end
+    it 'una lambda cualquiera no deberia tener el metodo and' do
+      expect { lambda {}.and(lambda {})}.to raise_error NoMethodError
+    end
+  end
 
-      it 'type(Symbol).call(:a) debe ser true' do
-        expect(type(Symbol).call(:a)).to be true
-      end
+  describe 'symbol-test' do
+    let(:symbol_dictionary) {Hash.new}
 
-      it 'type(Module).call(Comparable) debe ser true' do
-        expect(type(Module).call(Comparable)).to be true
-      end
+    it ':a.call("b") debe andar y devolver true' do
+      expect(:a.call("b")).to be true
     end
 
-    describe 'list-test' do
-      let(:array_pattern1) {[1, 2, 3, 4]}
-      let(:array_pattern2) {[1, 2, 3]}
-      let(:array_pattern3) {[:a, :b, :c, :d]}
-      let(:array_pattern4) {[:a, 2, :b, :c]}
-      let(:array_pattern5) {[type(Fixnum), :a, val(3)]}
+    it 'deberia obtener el diccionario que contiene :a=>2' do
+      :a.call(2, symbol_dictionary)
+      expect(symbol_dictionary).to be_an_eql({:a=>2})
+    end
+  end
 
-      it 'al pasarle algo que no es una lista deberia dar false' do # TODO : QUE PASA CON LOS HASHES Y LAS DEMAS COSAS ?
-        expect(list([],false).call(Module)).to be false
-      end
-      it 'al pasarle un array de cuatro siendo que espera ese mismo y tiene en cuenta el tamanio,
+  describe 'val-test' do
+    let(:symbol_dictionary) {Hash.new}
+
+    it 'val(5).call(2) debe dar falso' do
+      expect(val(5).call(2)).to be false
+    end
+
+    it 'val(5).call(5) debe dar true' do
+      expect(val(5).call(5)).to be true
+    end
+
+    it 'val(5).call(char 5) deberia dar falso' do
+      expect(val(5).call('5')).to be false
+    end
+
+    it 'deberia obtener el diccionario vacio' do
+      val(3).call(3, symbol_dictionary)
+      expect(symbol_dictionary).to be_empty
+    end
+
+    it 'deberia obtener el diccionario que contiene :a=>2' do
+      val(:a).call(1, symbol_dictionary)
+      expect(symbol_dictionary).to be_an_eql({:a=>1})
+    end
+  end
+
+  describe 'type-test' do
+    it 'type(Symbol).call(2) debe ser false' do
+      expect(type(Symbol).call(2)).to be false
+    end
+
+    it 'type(Symbol).call(:a) debe ser true' do
+      expect(type(Symbol).call(:a)).to be true
+    end
+
+    it 'type(Module).call(Comparable) debe ser true' do
+      expect(type(Module).call(Comparable)).to be true
+    end
+  end
+
+  describe 'list-test' do
+    let(:array_pattern1) {[1, 2, 3, 4]}
+    let(:array_pattern2) {[1, 2, 3]}
+    let(:array_pattern3) {[:a, :b, :c, :d]}
+    let(:array_pattern4) {[:a, 2, :b, :c]}
+    let(:array_pattern5) {[type(Fixnum), :a, val(3)]}
+
+    it 'al pasarle algo que no es una lista deberia dar false' do # TODO : QUE PASA CON LOS HASHES Y LAS DEMAS COSAS ?
+      expect(list([],false).call(Module)).to be false
+    end
+    it 'al pasarle un array de cuatro siendo que espera ese mismo y tiene en cuenta el tamanio,
           deberia devolver true' do
-        p list(array_pattern1).obtain_expected
-        expect(list(array_pattern1, true).call([1, 2, 3, 4])).to be true # es un objeto distinto a proposito
-      end
+      expect(list(array_pattern1, true).call([1, 2, 3, 4])).to be true # es un objeto distinto a proposito
+    end
 
-      it 'al pasarle un array de cuatro siendo que espera ese mismo y NO tiene en cuenta el tamanio,
+    it 'al pasarle un array de cuatro siendo que espera ese mismo y NO tiene en cuenta el tamanio,
           deberia devolver true' do
-        expect(list(array_pattern1, false).call([1, 2, 3, 4])).to be true # es un objeto distinto a proposito
-      end
+      expect(list(array_pattern1, false).call([1, 2, 3, 4])).to be true # es un objeto distinto a proposito
+    end
 
-      it 'al pasarle lo que espera pero al reves deberia ser false, sin importar el tamanio,
+    it 'al pasarle lo que espera pero al reves deberia ser false, sin importar el tamanio,
           porque sino no anda bien la busqueda de los primeros N elementos' do
-        expect(list(array_pattern1, true).call([4,3,2,1])).to be false
-      end
+      expect(list(array_pattern1, true).call([4,3,2,1])).to be false
+    end
 
-      it 'al pasarle un array con todos symbols deberia dar true' do
-        expect(list(array_pattern3, true).call([1,2,3,4])).to be true
-      end
+    it 'al pasarle un array con todos symbols deberia dar true' do
+      expect(list(array_pattern3, true).call([1,2,3,4])).to be true
+    end
 
-      it 'al pasarle un array con symbols y numeros deberia dar true, cuando se evalua con un array con los elementos en el orden esperado' do
-        expect(list(array_pattern4, true).call([1,2,3,4])).to be true
-      end
+    it 'al pasarle un array con symbols y numeros deberia dar true, cuando se evalua con un array con los elementos en el orden esperado' do
+      expect(list(array_pattern4, true).call([1,2,3,4])).to be true
+    end
 
-      it 'al pasarle un array con symbols y numeros (o solo symbols) teniendo en cuenta el tamanio y evaluarlo
+    it 'al pasarle un array con symbols y numeros (o solo symbols) teniendo en cuenta el tamanio y evaluarlo
          con un array mas chico deberia dar false' do
-        expect(list(array_pattern4, true).call([1,2,3])).to be false
-      end
-
-      it 'si no se especifica match size no deberia romper y deberia considerarlo true, y este test deberia dar true' do
-        expect(list(array_pattern4).call([1,2,3,4])).to be true
-      end
-
-      it 'al pasarle una lista con matchers deberia andar' do
-        expect(list(array_pattern5).call([1,2,3])).to be true
-      end
+      expect(list(array_pattern4, true).call([1,2,3])).to be false
     end
 
-    describe 'duck-test' do
-      let(:some_array) { Array.new }
-
-      it 'un array entiende each' do
-        expect(duck(:each).call(some_array)).to be true
-      end
-
-      it 'un array entiende each, all?' do
-        expect(duck(:each, :all?).call(some_array)).to be true
-      end
-
-      it 'un array entiende each, all? y any?' do
-        expect(duck(:each, :all?, :any?).call(some_array)).to be true
-      end
-
-      it 'un array no entiende salto_ninja' do
-        expect(duck(:salto_ninja).call(some_array)).to be false
-      end
-
-      it 'un array entiende any? y all? pero no entiende salto_ninja, debe dar false' do
-        expect(duck(:any?, :all?, :salto_ninja).call(some_array)).to be false
-      end
+    it 'si no se especifica match size no deberia romper y deberia considerarlo true, y este test deberia dar true' do
+      expect(list(array_pattern4).call([1,2,3,4])).to be true
     end
+
+    it 'al pasarle una lista con matchers deberia andar' do
+      expect(list(array_pattern5).call([1,2,3])).to be true
+    end
+  end
+
+  describe 'duck-test' do
+    let(:some_array) { Array.new }
+
+    it 'un array entiende each' do
+      expect(duck(:each).call(some_array)).to be true
+    end
+
+    it 'un array entiende each, all?' do
+      expect(duck(:each, :all?).call(some_array)).to be true
+    end
+
+    it 'un array entiende each, all? y any?' do
+      expect(duck(:each, :all?, :any?).call(some_array)).to be true
+    end
+
+    it 'un array no entiende salto_ninja' do
+      expect(duck(:salto_ninja).call(some_array)).to be false
+    end
+
+    it 'un array entiende any? y all? pero no entiende salto_ninja, debe dar false' do
+      expect(duck(:any?, :all?, :salto_ninja).call(some_array)).to be false
+    end
+  end
 end
 
 describe 'combinations tests' do
@@ -131,9 +148,6 @@ describe 'combinations tests' do
 
   describe 'and-test' do
     it 'deberia poder combinar dos matchers sencillos con and y deberia devolver true al evaluarlos' do
-      #evaluator = type(Integer).and(val(3))
-      #evaluator.call(3)
-      #p 'Contenido del diccionario: ' + evaluator.symbol_dictionary
       expect(type(Integer).and(val(3)).call(3)).to be true
     end
 
