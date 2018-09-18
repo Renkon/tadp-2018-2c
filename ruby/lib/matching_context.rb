36cc5463fd065e99ec7d5f078e42df17bbd8ba11
Fernando Martín Besteiro <fmbesteiro@gmail.com>
2018-09-18 16:30:36 -0300

    Removí accessors innecesarios

--- ruby/lib/matching_context.rb
+++ ruby/lib/matching_context.rb
@@ -2,25 +2,23 @@ require_relative "disposable_context"
 
 # Extra class useful for our environment
 class MatchingContext
-  attr_accessor :__object__
-  attr_accessor :__return_proc__
 
   def initialize(object, return_proc)
-    @__object__ = object
-    @__return_proc__ = return_proc
+    @object = object
+    @return_proc = return_proc
   end
 
   # This method is used to define which matchers should be analyzed to execute a chunk of code
   def with(initial_matcher, *matchers, &block)
     matcher_proc = matchers.length > 0 ? initial_matcher.and(*matchers) : initial_matcher
     disposable_context = DisposableContext.new
-    if matcher_proc.call(__object__, disposable_context)
-      @__return_proc__.call disposable_context.instance_eval(&block)
+    if matcher_proc.call(@object, disposable_context)
+      @return_proc.call disposable_context.instance_eval(&block)
     end
   end
 
   def otherwise(&block)
     disposable_context = DisposableContext.new
-    @__return_proc__.call disposable_context.instance_eval(&block)
+    @return_proc.call disposable_context.instance_eval(&block)
   end
 end
