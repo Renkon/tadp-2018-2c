b124a4379c8564b83bf5e73f9f7d8b4d49f48cd5
Lucas <lucasleonardo1996@hotmail.com>
2018-11-01 15:07:28 -0300

    Agrego un test mas para los casos de cualquier otra raza en cargarKi

--- scala/src/test/scala/ProjectSpec.scala
+++ scala/src/test/scala/ProjectSpec.scala
@@ -8,16 +8,21 @@ class ProjectSpec extends FreeSpec with Matchers {
 
       val goku = new Saiyajin(nombre ="goku", energia = 40, tieneCola = true, nivelSS = 0, energiaMaxima = 300)
       val androide17 = new Androide(nombre = "androide 17", energiaMaxima = 300)
+      val yamcha = new Humano(nombre = "yamcha", energia = 25, energiaMaxima = 200)
 
       "cuando un guerrero saiyajin con 40 de energia inicial y nivelSS = 1 carga ki entonces su eneria es la original mas 150" in {
-        val energiaOriginal : Int = goku.energia
         val gokuConMasKi = CargarKi(goku)
-        gokuConMasKi.energia shouldBe(energiaOriginal + 150)
+        gokuConMasKi.energia shouldBe(goku.energia + 150)
       }
 
       "cuando un androide intenta cargarKi su energia permanece igual" in {
         val androideConMasKi = CargarKi(androide17)
         androideConMasKi.energia shouldBe(androide17.energia)
+      }
+
+      "cuando un humano intenta cargarKi su energia aumenta en 100" in {
+        val yamchaConMasKi = CargarKi(yamcha)
+        yamchaConMasKi.energia shouldBe(yamcha.energia + 100)
       }
     }
   }
